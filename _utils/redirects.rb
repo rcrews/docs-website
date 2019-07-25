82b1f8e0c649f5a0d9c7eb22a11cdeb0374a8587
Robert Crews <rcrews@cloudera.com>
2019-07-25 14:20:53 -0700

    Update redirects

---
+++ _utils/redirects.rb
@@ -0,0 +1,23 @@
+#!/usr/env ruby -w
+# frozen_string_literal: true
+
+# Create client-side redirects for www.cloudera destinations referenced
+# from (legacy) cloudera documentation. Whenever a link starts with a
+# slash (under the expection the URL references a resource on the
+# www.cloudera.com site) we can redirect to the equivalent
+# www.cloudera.com resource.
+
+require 'erb'
+require 'fileutils'
+require 'yaml'
+
+redirects = YAML.load(File.read('_utils/redirects.yml'))
+
+template = '<meta http-equiv="refresh" content="0; URL=<%= @destination %>">'
+
+redirects.each do |redirect|
+  @destination = redirect['d']
+  html = ERB.new(template, trim_mode: '%<>')
+  FileUtils.mkdir_p File.dirname(redirect['s'])
+  File.write(redirect['s'], html.result)
+end
