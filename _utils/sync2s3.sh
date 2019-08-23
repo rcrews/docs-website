53a0523b8c056122d5f32bebd1a8e5d7cf7104cc
Robert Crews <rcrews@cloudera.com>
2019-08-23 08:49:39 -0700

    Update sync

--- _utils/sync2s3.sh
+++ _utils/sync2s3.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-cd /Home/rcrews/Sandbox/docs-website
+cd /Users/rcrews/Sandbox/docs-website
 git checkout dev
 git pull
 bundle exec jekyll build
 aws s3 sync --size-only _site/ s3://docs-dev.cloudera.com
 
-cd /Home/rcrews/Sandbox/docs-website
+cd /Users/rcrews/Sandbox/docs-website
 git checkout playbranch
 git pull public playbranch
 git pull cloudera playbranch
 bundle exec jekyll build
 aws s3 sync --size-only _site/ s3://docs-stage.cloudera.com
 
-cd /Home/rcrews/Sandbox/docs-website
+cd /Users/rcrews/Sandbox/docs-website
 git checkout master
 git pull
 bundle exec jekyll build
 aws s3 sync --size-only _site/ s3://docs.hortonworks.com
 
-cd /Home/rcrews/Sandbox/docs-website
+cd /Users/rcrews/Sandbox/docs-website
 git checkout prod
 git pull
 bundle exec jekyll build
