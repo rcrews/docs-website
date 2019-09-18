4b51fef96b6691a7dea6ec54970fa95a80448eb3
Robert Crews <rcrews@cloudera.com>
2019-09-18 12:43:06 -0700

    Script change

--- _utils/sync2s3.sh
+++ _utils/sync2s3.sh
@@ -5,21 +5,21 @@ git fetch public playbranch
 
 _utils/switchsite.rb stage
 RESULT=$?
-if [[ RESULT == 0 ]] ; then
+if [[ RESULT != 1 ]] ; then
     bundle exec jekyll build
     java -jar ../s3sync/s3sync.jar upload --directory _site --bucket 'docs-stage.cloudera.com'
 fi
 
 _utils/switchsite.rb dev
 RESULT=$?
-if [[ RESULT == 0 ]] ; then
+if [[ RESULT != 1 ]] ; then
     bundle exec jekyll build
     java -jar ../s3sync/s3sync.jar upload --directory _site --bucket 'docs-dev.cloudera.com'
 fi
 
 _utils/switchsite.rb prod
 RESULT=$?
-if [[ RESULT == 0 ]] ; then
+if [[ RESULT != 1 ]] ; then
     bundle exec jekyll build
     java -jar ../s3sync/s3sync.jar upload --directory _site --bucket 'docs.cloudera.com'
 fi
