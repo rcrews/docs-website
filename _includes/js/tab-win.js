22d1c9caf17163b3c2f3a7d14654bdee483ec661
Jason Davis <jason.davis@cloudera.com>
2019-10-21 16:03:51 -0700

    Refactor tabbedWindows function

--- _includes/js/tab-win.js
+++ _includes/js/tab-win.js
@@ -1,29 +1,20 @@
-function tabbedWindows(evt, env) {
+const tabbedWindows = evt => {
+  const contentId = evt.currentTarget.getAttribute('data-target');
 
-  // Get all elements with class="tabcontent" and hide them
-  let tabcontent = document.getElementsByClassName("tabcontent");
-  for (let i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+  // Get all elements with class="tabcontent" and hide them, and show the current tab
+  document.querySelectorAll(".tabcontent").forEach(tabContent => {
+    tabContent.style.display = tabContent.id === contentId ? 'block' : 'none';
+  });
 
   // Get all elements with class="tablinks" and remove the class "active"
-  let tablinks = document.getElementsByClassName("tablinks");
-  for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+  document.querySelectorAll(".tablinks").forEach(tablink => {
+    tablink.classList.remove('active');
+  });
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(env).style.display = "block";
-  evt.currentTarget.className += " active";
+  // Add an "active" class to the button that opened the tab
+  evt.currentTarget.classList.add('active');
 }
 
-const tab = document.querySelectorAll(".tablinks");
-const content = document.querySelectorAll(".tabcontent");
-
-for (let i = 0; i < tab.length; i++) {
-    let conId = tab[i].getAttribute('data-target');
-    let conArray = Array.from(content);
-    let con = conArray.find((c) => c.getAttribute('id') === conId);
-    let c = con.getAttribute('id');
-    tab[i].addEventListener('click', (event) => tabbedWindows(event,c), false);
-}
+document.querySelectorAll(".tablinks").forEach(tab => {
+  tab.addEventListener('click', tabbedWindows, false);
+});
