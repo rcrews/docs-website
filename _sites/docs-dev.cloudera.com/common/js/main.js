4a51f1a0a6e8c06396d79c61c6918d9d055ebd4b
Robert Crews <rcrews@cloudera.com>
2019-10-30 09:51:01 -0700

    Further isolate bad JavaScript

--- _sites/docs-dev.cloudera.com/common/js/main.js
+++ _sites/docs-dev.cloudera.com/common/js/main.js
@@ -2,6 +2,6 @@
 ---
 // Use Jekyll to bundle JS files.
 {% include js/jquery-3.4.1.min.js %}
-{% include js/tab-win.js %}
 // Jason's file is only on dev for now
+{% include js/tab-win.js %}
 {% include js/newux.js %}
--- common/js/main.js
+++ common/js/main.js
@@ -2,4 +2,4 @@
 ---
 // Use Jekyll to bundle JS files.
 {% include js/jquery-3.4.1.min.js %}
-{% include js/tab-win.js %}
+{% include js/newux.js %}
