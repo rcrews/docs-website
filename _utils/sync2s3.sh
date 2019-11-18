b987fecc852c73fd1584d75b5ed8a150bc37dcaa
Robert Crews <rcrews@cloudera.com>
2019-11-18 06:45:40 -0800

    Clear _site between builds to assure the wrong content is not published

--- _utils/sync2s3.sh
+++ _utils/sync2s3.sh
@@ -37,6 +37,7 @@ require_clean_work_tree
 _utils/switchsite.rb stage
 RESULT=$?
 if [[ RESULT != 1 ]] ; then
+    rm -rf _site/*
     bundle exec jekyll build --trace
     java -jar ../s3sync/s3sync.jar upload --directory _site --bucket 'docs-stage.cloudera.com'
 
@@ -56,6 +57,7 @@ fi
 _utils/switchsite.rb dev
 RESULT=$?
 if [[ RESULT != 1 ]] ; then
+    rm -rf _site/*
     bundle exec jekyll build --trace
     java -jar ../s3sync/s3sync.jar upload --directory _site --bucket 'docs-dev.cloudera.com'
 fi
@@ -63,6 +65,7 @@ fi
 _utils/switchsite.rb prod
 RESULT=$?
 if [[ RESULT != 1 ]] ; then
+    rm -rf _site/*
     bundle exec jekyll build --trace
     java -jar ../s3sync/s3sync.jar upload --directory _site --bucket 'docs.cloudera.com'
 fi
