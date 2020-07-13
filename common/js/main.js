fd0ff2004dec053029a5f52b94534c85966b10ac
Robert Crews <rcrews@cloudera.com>
2020-07-13 12:53:13 -0700

    Add updated main.js

--- common/js/main.js
+++ common/js/main.js
@@ -1,8 +1,12 @@
 ---
 ---
-// Use Jekyll to bundle JS files.
+// Bundle JavaScript files
 {% include js/jquery-3.4.1.min.js %}
 {% include js/cdoc-filter.js %}
 {% include js/cdoc-utils.js %}
+{% include js/newux-who-am-i.js %}
+{% include js/newux-pub-nav.js %}
+{% include js/newux-search.js %}
+{% include js/newux-utils.js %}
+{% include js/newux-transforms.js %}
 {% include js/newux.js %}
-
